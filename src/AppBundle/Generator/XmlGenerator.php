1b6862e49286ccb21c4566de5466141c4d2feeb2
Mantas Marcinkevičius <marc.mantas@gmail.com>
2018-01-24 22:38:52 +0200

    finished the use case xmi creation

--- src/AppBundle/Generator/XmlGenerator.php
+++ src/AppBundle/Generator/XmlGenerator.php
@@ -4,7 +4,15 @@ namespace AppBundle\Generator;
 
 class XmlGenerator
 {
+    /**
+     * @var string
+     */
     private $initial = '<?xml version="1.0" encoding="UTF-8"?><uml:Model xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:uml="http://www.eclipse.org/uml2/5.0.0/UML" xmi:id="_diagram" name="model">';
+
+    /**
+     * @var int
+     */
+    private $associationCount = 0;
 
     /**
      * @param array $data The necessary data for UML diagram creation
@@ -27,7 +35,8 @@ class XmlGenerator
         }
 
         $xml .= '</uml:Model>';
-        echo $xml;
+
+        return $xml;
     }
 
     /**
@@ -53,15 +62,8 @@ class XmlGenerator
             $this->getIdisifiedString($actor),
             $actor
             );
-        $xml .= sprintf(
-            '<eAnnotations xmi:id="%s" source="visual-scrum">' . PHP_EOL,
-            $this->getIdisifiedString($actor) . '_annotation'
-        );
-        $xml .= sprintf(
-            '<details xmi:id="%s" key="uuid" value="%s"/>' . PHP_EOL,
-            $this->getIdisifiedString($actor) . '_details',
-            uniqid('_')
-        );
+
+        $this->addAnnotations($this->getIdisifiedString($actor), $xml);
         $xml .= "</eAnnotations>\n</packagedElement>\n";
     }
 
@@ -76,15 +78,8 @@ class XmlGenerator
             $this->getIdisifiedString($useCase),
             $useCase
         );
-        $xml .= sprintf(
-            '<eAnnotations xmi:id="%s" source="visual-scrum">' . PHP_EOL,
-            $this->getIdisifiedString($useCase) . '_annotation'
-        );
-        $xml .= sprintf(
-            '<details xmi:id="%s" key="uuid" value="%s"/>' . PHP_EOL,
-            $this->getIdisifiedString($useCase) . '_details',
-            uniqid('_')
-        );
+
+        $this->addAnnotations($this->getIdisifiedString($useCase), $xml);
         $xml .= "</eAnnotations>\n</packagedElement>\n";
     }
 
@@ -95,25 +90,105 @@ class XmlGenerator
      */
     private function addAssociationNode(string $useCase, string $actor, string &$xml): void
     {
+        $end1 = 'assoc_' . $this->associationCount . '_end_1';
+        $end2 = 'assoc_' . $this->associationCount . '_end_2';
+
         $xml .= sprintf(
-            '<packagedElement xsi:type="uml:UseCase" xmi:id="%s" name="%s">' . PHP_EOL,
+            '<packagedElement xsi:type="uml:Association" xmi:id="%s" name="%s" memberEnd="%s" navigableOwnedEnd="%s">' . PHP_EOL,
+            'association_' . ++$this->associationCount,
+            'Association ' . $this->associationCount,
+            "$end1 $end2",
+            "$end2 $end1"
+        );
+
+        $this->addAnnotations('association_' . $this->associationCount, $xml);
+        $xml .= "</eAnnotations>\n";
+
+        $xml .= sprintf(
+            '<ownedEnd xmi:id="%s" name="useCase" type="%s" association="%s">',
+            $end1,
             $this->getIdisifiedString($useCase),
-            $useCase
+            'association_' . $this->associationCount
         );
+
+        $this->addAnnotations($end1, $xml);
+        $xml .= "</eAnnotations>\n";
+
+        // <upperValue xsi:type="uml:LiteralInteger" xmi:id="_yIR09vyVEeeyruBoe7-QtQ" value="1">
         $xml .= sprintf(
-            '<eAnnotations xmi:id="%s" source="visual-scrum">' . PHP_EOL,
-            $this->getIdisifiedString($useCase) . '_annotation'
+            '<lowerValue xmi:id="%s" xsi:type="uml:LiteralInteger">',
+            $end1 . '_lower'
         );
+
+        $this->addAnnotations($end1 . '_lower', $xml);
+        $xml .= "</eAnnotations>\n";
+        $xml .= "</lowerValue>\n";
+
         $xml .= sprintf(
-            '<details xmi:id="%s" key="uuid" value="%s"/>' . PHP_EOL,
-            $this->getIdisifiedString($useCase) . '_details',
-            uniqid('_')
+            '<upperValue xmi:id="%s" xsi:type="uml:LiteralInteger" value="1">',
+            $end1 . 'upper'
         );
-        $xml .= "</eAnnotations>\n</packagedElement>\n";
+
+        $this->addAnnotations($end1 . '_upper', $xml);
+        $xml .= "</eAnnotations>\n";
+        $xml .= "</upperValue>\n";
+        $xml .= "</ownedEnd>\n";
+
+        $xml .= sprintf(
+            '<ownedEnd xmi:id="%s" name="useCase" type="%s" association="%s">',
+            $end2,
+            $this->getIdisifiedString($actor),
+            'association_' . $this->associationCount
+        );
+
+        $this->addAnnotations($end1, $xml);
+        $xml .= "</eAnnotations>\n";
+
+        $xml .= sprintf(
+            '<lowerValue xmi:id="%s" xsi:type="uml:LiteralInteger">',
+            $end2 . '_lower'
+        );
+
+        $this->addAnnotations($end2 . '_lower', $xml);
+        $xml .= "</eAnnotations>\n";
+        $xml .= "</lowerValue>\n";
+
+        $xml .= sprintf(
+            '<upperValue xmi:id="%s" xsi:type="uml:LiteralInteger" value="1">',
+            $end1 . 'upper'
+        );
+
+        $this->addAnnotations($end1 . '_upper', $xml);
+        $xml .= "</eAnnotations>\n";
+        $xml .= "</upperValue>\n";
+        $xml .= "</ownedEnd>\n";
+        $xml .= "</packagedElement>\n";
     }
 
+    /**
+     * @param string $string
+     *
+     * @return string
+     */
     private function getIdisifiedString(string $string): string
     {
         return str_replace(' ', '_', strtolower($string));
+    }
+
+    /**
+     * @param string $element
+     * @param string $xml
+     */
+    private function addAnnotations(string $element, string &$xml)
+    {
+        $xml .= sprintf(
+            '<eAnnotations xmi:id="%s" source="visual-scrum">' . PHP_EOL,
+            $element . '_annotation'
+        );
+        $xml .= sprintf(
+            '<details xmi:id="%s" key="uuid" value="%s"/>' . PHP_EOL,
+            $element . '_details',
+            uniqid('_')
+        );
     }
 }
